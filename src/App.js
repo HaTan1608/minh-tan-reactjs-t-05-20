e61002e573e835ad7afdd79958e06d3dce6f16cd
tandev94 <56614953+tandev94@users.noreply.github.com>
2020-05-20 09:06:52 +0700

    hâh

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>123456</code> and save to reload.
+          Tài óc chó ^^
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.facebook.com/taihoangthanh79"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn ReactJS
+          Facebook of Tài
         </a>
       </header>
     </div>
